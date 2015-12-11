585894478c54cbaf1974689b0d36c06d3e0ccfe1
Ben Balter <ben.balter@github.com>
2015-12-11 10:41:51 -0500

    TEST ALL THE THINGS!

--- test/test_fortune-finder.rb
+++ test/test_fortune-finder.rb
@@ -1,6 +1,41 @@
 require 'helper'
 
 class TestFortuneFinder < Test::Unit::TestCase
+
+  def cleanup
+    file = File.expand_path "symlink.com.toml", FortuneFinder.domains_path
+    FileUtils.rm file if File.exists? file
+  end
+
+  should "know the domains path" do
+    expected = File.expand_path "../lib/data/2015", File.dirname(__FILE__)
+    assert_equal expected, FortuneFinder.domains_path
+  end
+
+  should "lookup a domain via the class method" do
+    record = FortuneFinder.lookup 'att.com'
+    assert_equal 12, record.rank
+    assert_equal 'AT&T', record.name
+  end
+
+  should "know if a domain is valid via the class method" do
+    assert FortuneFinder.valid? 'att.com'
+    assert FortuneFinder.ranked? 'att.com'
+  end
+
+  should "know if a domain exists via the instance method" do
+    finder = FortuneFinder.new('att.com')
+    assert finder.ranked?
+    assert finder.valid?
+  end
+
+  should "lookup a record via the class method" do
+    finder = FortuneFinder.new('att.com')
+    record = finder.lookup
+    assert_equal 12, record.rank
+    assert_equal 'AT&T', record.name
+  end
+
   should "get a response for a domain name" do
     record = FortuneFinder::lookup('att.com')
     assert_equal 12, record.rank
@@ -8,8 +43,18 @@ class TestFortuneFinder < Test::Unit::TestCase
   end
 
   should "know when a domain is an alias" do
-    record = FortuneFinder::lookup('ebay.com')
-    assert_equal true, record.alias
-    assert_equal 'ebayinc.com', record.domain
+    to = File.expand_path "symlink.com.toml", FortuneFinder.domains_path
+    from = File.expand_path "walmart.com.toml", FortuneFinder.domains_path
+    FileUtils.ln_s(from, to) unless File.exists? to
+
+    record = FortuneFinder::lookup('symlink.com')
+    assert_equal true, record.alias?
+    assert_equal 'walmart.com', record.domain
+  end
+
+  should "return all records" do
+    record = FortuneFinder.all.first
+    assert_equal 1, record.rank
+    assert_equal 'Walmart', record.name
   end
 end
